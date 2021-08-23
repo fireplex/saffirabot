bc74948db91cd9c96cb1a88f498becf7ee3ea549
feeka <ampersand2909@gmail.com>
2021-08-23 13:53:25 +0100

    Custom FA scraper implemented
    Added better functionality using Puppeteer to create our own FA scraper. Now allows the use of sessions to enable scraping NSFW pages

--- events/message.js
+++ events/message.js
@@ -2,25 +2,47 @@ module.exports = (client, message) => {
 
 	if (message.content.includes("furaffinity.net/view/")) {
 		console.log("FA link");
-		const FurAffinityClient = require("fa.js").FurAffinityClient;
-		const fa = new FurAffinityClient("b=XXX; a=XXX; s=1");
+		var urlRegex = /(https?:\/\/[^ ]*)/;
+		var url = message.content.match(urlRegex)[1];
+		const puppeteer = require('puppeteer');
 
-		var subID = message.content.split("www.furaffinity.net/view/")[1];
-		var subURL = message.content;
-		subID = subID.slice(0, -1); 
+		(async () => {
+			const browser = await puppeteer.launch({
+				executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+				headless: true,
+				ignoreDefaultArgs: ['--disable-extensions'],
+				args: [
+				'--user-data-dir=C:\\Users\\Feeka\\AppData\\Local\\Google\\Chrome\\User Data'
+				],
+			});
+			const page = await browser.newPage();
+			await page.goto(url);
+	
+			var sub = await page.evaluate(() => {
+				var views = document.getElementsByClassName("views")[0].getElementsByTagName('span')[0].innerText.toString();
+				var comments = document.getElementsByClassName("comments")[0].getElementsByTagName('span')[0].innerText.toString();
+				var favs = document.getElementsByClassName("favorites")[0].getElementsByTagName('span')[0].innerText.toString();
+				var avatar = document.getElementsByClassName("submission-user-icon")[0].src;
+				var author = document.getElementsByClassName("submission-id-sub-container")[0].getElementsByTagName('a')[0].innerText.toString();
+				var date = document.getElementsByClassName("popup_date")[0].innerText.toString();
+				var url = document.getElementById("submissionImg").src;
+				var title = document.getElementsByClassName("submission-title")[0].getElementsByTagName("p")[0].innerText.toString();
+				var subCont = {views: views, comments: comments, favs: favs, avatar: avatar, author: author, date: date, url: url, title: title};
+	
+				return subCont;
+			});
+			message.delete();
 
-		fa.getSubmission(subID).then((sub) => {
-			if(sub.url != "" && sub.title != "") {
-				message.delete();
-				const embed = new client.embed();
-				embed.setAuthor(sub.artist, sub.artist_thumb);
-				embed.setImage(sub.url);
-				embed.setTitle(sub.title);
-				embed.setURL(subURL);
-				embed.setFooter(`Created ${sub.when} | ${sub.when_title}`);
-				message.channel.send({ content: '** **', embeds: [embed] });
-			}
-		});
+			const embed = new client.embed();
+			embed.setAuthor(sub.author, sub.avatar);
+			embed.setImage(sub.url);
+			embed.setTitle(sub.title);
+			embed.setURL(sub.url);
+			embed.setFooter(`Views: ${sub.views} | Favs: ${sub.favs} | Comments: ${sub.comments} || Created ${sub.date}`);
+			message.channel.send({ content: '** **', embeds: [embed] });
+
+			await browser.close();
+		})();
 	}
 
 	if (message.author.bot) return;
