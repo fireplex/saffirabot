4a7db5d4fcfa02b0606395548e791d3f11df07b9
feeka <ampersand2909@gmail.com>
2021-09-26 10:41:42 +0100

    PM2 Dash IO

--- events/interaction.js
+++ events/interaction.js
@@ -1,10 +1,17 @@
 module.exports = (client, interaction) => {
-  if (!interaction.isCommand()) return;
+	const io = require('@pm2/io');
+
+	const currentInteractions = io.counter({
+		name: 'Realtime interactions count',
+		id: 'bot/realtime/interactions'
+	});
+	if (!interaction.isCommand()) return;
 
 	const command = interaction.commandName;
-  const cmd = client.commands.get(command);
+	const cmd = client.commands.get(command);
 	
 	if (!cmd) return;
 	
+	currentInteractions.inc();
 	cmd.run(client, interaction);
 }
