a225d4ba000d6663436db14094798b2be6a44be1
fireplex <ampersand2909@gmail.com>
2021-11-28 12:51:39 +0000

    FirrbaseAppError Fix 1
    Attempt to fix, too lazy to test locally!

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,14 +1,14 @@
 exports.run = (client, interaction) => {
 	var admin = require("firebase-admin");
 	var serviceAccount = require("./pk.json");
-	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount),
-		databaseURL: "https://saffira-bot-default-rtdb.europe-west1.firebasedatabase.app"
-	});
 	
 	var db = admin.database();
 	if(interaction.options.getSubcommand() === "add") {
 		interaction.deferReply().then(() => {
+			admin.initializeApp({
+				credential: admin.credential.cert(serviceAccount),
+				databaseURL: "https://saffira-bot-default-rtdb.europe-west1.firebasedatabase.app"
+			});
 			function uniqueArray(arr) { //Remove duplicates
 				return arr.filter(function(item, index){
 					return arr.indexOf(item) >= index;
@@ -39,6 +39,10 @@ exports.run = (client, interaction) => {
 
 	if(interaction.options.getSubcommand() === "remove") {
 		interaction.deferReply().then(() => {
+			admin.initializeApp({
+				credential: admin.credential.cert(serviceAccount),
+				databaseURL: "https://saffira-bot-default-rtdb.europe-west1.firebasedatabase.app"
+			});
 			var ref = db.ref("restrictedRoles"); 
 			ref.once("value", function(snapshot) {
 				var json = JSON.parse(snapshot.val());
@@ -67,6 +71,10 @@ exports.run = (client, interaction) => {
 
 	if(interaction.options.getSubcommand() === "view") {
 		interaction.deferReply().then(() => {
+			admin.initializeApp({
+				credential: admin.credential.cert(serviceAccount),
+				databaseURL: "https://saffira-bot-default-rtdb.europe-west1.firebasedatabase.app"
+			});
 			var ref = db.ref("restrictedRoles"); 
 			ref.once("value", function(snapshot) {
 				var json = JSON.parse(snapshot.val());
