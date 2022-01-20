dfc10d5763194181c07266445d78e11822a01d9a
fireplex <ampersand2909@gmail.com>
2022-01-20 20:38:29 +0000

    Wrong guild ID fix

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,6 +1,6 @@
 module.exports = (client, oldMember, newMember) => {
 	if(newMember.isCommunicationDisabled()) {
-		client.guilds.cache.find(guild => guild.id === "516779786811473939")
+		client.guilds.cache.find(guild => guild.id === "809214093163626527")
 			.channels.cache.find(channel => channel.name === "logs")
 			.send(`\`${newMember.displayName} was timed out.\``)
 			.catch(console.error);
