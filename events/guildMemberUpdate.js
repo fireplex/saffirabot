319c6c4eb58790025f9f29dbe658a3820e38d221
fireplex <ampersand2909@gmail.com>
2022-02-15 22:45:12 +0000

    Disable timeout logging
    Timeout logging causing critical errors, will fix.

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,8 +1,8 @@
 module.exports = (client, oldMember, newMember) => {
-	if(newMember.isCommunicationDisabled()) {
+	/*if(newMember.isCommunicationDisabled()) {
 		client.guilds.cache.find(guild => guild.id === "809214093163626527")
 			.channels.cache.find(channel => channel.name === "logs")
 			.send(`\`${newMember.displayName} was timed out.\``)
 			.catch(console.error);
-	}
+	}*/
 }
